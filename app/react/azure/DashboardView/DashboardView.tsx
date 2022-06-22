cfcd8f2a2bab85f86610f3a1d1ce99db0b3df560
Chaim Lev-Ari <chiptus@gmail.com>
2022-06-22 08:03:32 +0300

    fix(azure): remove optional chain

--- app/react/azure/DashboardView/DashboardView.tsx
+++ app/react/azure/DashboardView/DashboardView.tsx
@@ -16,7 +16,7 @@ export function DashboardView() {
     subscriptionsQuery.data
   );
 
-  const subscriptionsCount = subscriptionsQuery?.data?.length;
+  const subscriptionsCount = subscriptionsQuery.data?.length;
   const resourceGroupsCount = Object.values(
     resourceGroupsQuery.resourceGroups
   ).flatMap((x) => Object.values(x)).length;
