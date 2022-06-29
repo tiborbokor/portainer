a4db0e5dc692204935a5573a6b0638c7c6d5fc3f
Prabhat Khera <prabhat.khera@portainer.io>
2022-06-29 12:37:19 +1200

    add ui update to kube add application

--- app/react/components/Icon.tsx
+++ app/react/components/Icon.tsx
@@ -12,9 +12,10 @@ interface Props {
   icon: ReactNode | ComponentType<unknown>;
   feather?: boolean;
   className?: string;
+  mode?: 'info' | 'warn' | 'error' | 'success';
 }
 
-export function Icon({ icon, feather, className }: Props) {
+export function Icon({ icon, feather, className, mode }: Props) {
   useEffect(() => {
     if (feather) {
       featherIcons.replace();
@@ -31,11 +32,16 @@ export function Icon({ icon, feather, className }: Props) {
     );
   }
 
+  let classes = clsx(className, `icon`);
+  if (mode) {
+    classes = clsx(className, `icon icon-${mode}`);
+  }
+
   if (feather) {
     return (
       <i
         data-feather={icon}
-        className={className}
+        className={classes}
         aria-hidden="true"
         role="img"
       />
@@ -43,6 +49,6 @@ export function Icon({ icon, feather, className }: Props) {
   }
 
   return (
-    <i className={clsx('fa', icon, className)} aria-hidden="true" role="img" />
+    <i className={clsx('fa', icon, classes)} aria-hidden="true" role="img" />
   );
 }
